5722044e3652090d3beb3bc14c478cec706be138
Pablo Romero <107282403+pabloromeero@users.noreply.github.com>
2022-09-08 15:31:55 -0300

    Create strategy.js

---
+++ exercícios/strategy.js
@@ -0,0 +1,18 @@
+class soma {
+    execute(a, b){
+        return a + b
+    }
+}
+
+class subtração {
+    execute(a, b){
+        return a - b
+    }
+}
+
+class multiplição {
+    execute(a, b){
+        return a * b
+    }
+}
+
