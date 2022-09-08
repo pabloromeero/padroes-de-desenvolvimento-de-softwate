2725c3bb697c388032df8fd6705f3502840f9011
Pablo Romero <107282403+pabloromeero@users.noreply.github.com>
2022-09-08 17:23:30 -0300

    Create observer.js

---
+++ exercícios/observer.js
@@ -0,0 +1,11 @@
+class observable {
+    TextEditor(insertLine, text){
+        insertLine() = lineNumber & text
+        lineNumber = 'Inserido Texto'
+    } 
+    removeLine(lineNumber) {
+        return text - lineNumber
+    } 
+    openTextEditor()
+    saveTextEditor()
+}
